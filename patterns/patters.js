3079a9268f15bc1f8fe5cfe61ce1831444e89537
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-01-18 00:15:47 +0530

    Create patters.js

---
+++ patterns/patters.js
@@ -0,0 +1,120 @@
+
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+
+class Main {
+    public static void main(String[] args) {
+
+		for(int i= 1; i<= 5; i++) {
+			for(int j= 1; j<= i; j++) {
+				System.out.print(" * ");
+			}
+			System.out.println(" ");
+		}
+    }
+}
+
+*/
+
+
+
+
+
+/*
+1  
+0 1  
+1 0 1  
+0 1 0 1  
+1 0 1 0 1  
+
+
+class Main {
+    public static void main(String[] args) {
+
+		for(int i= 1; i<= 5; i++) {
+			for(int j= 1; j<= i; j++) {
+			    if((i+j) % 2 == 0){
+			        System.out.print(1+ " ");
+			    }else{
+			        System.out.print(0+" ");
+			    }
+			}
+			System.out.println(" ");
+		}
+    }
+}
+*/
+
+
+/*
+*  *  *  *  *  
+*  *  *  *  
+*  *  *  
+*  *  
+*  
+
+
+class Main {
+    public static void main(String[] args) {
+		for(int i= 5; i>= 1; i--) {
+			for(int j= i; j>= 1; j--) {
+			       System.out.print(" * ");
+			}   
+			System.out.println(" ");
+		}   
+    }
+}
+*/
+
+
+/*
+
+1  
+1 2  
+1 2 3  
+1 2 3 4  
+1 2 3 4 5  
+
+
+class Main {
+    public static void main(String[] args) {
+
+		for(int i= 1; i<= 5; i++) {
+			for(int j= 1; j<= i; j++) {
+				System.out.print(j+ " ");
+			}
+			System.out.println(" ");
+		}
+    }
+}
+
+*/
+
+/*
+
+A 
+BC 
+DEF 
+GHIJ 
+KLMNO 
+
+
+
+class Main {
+    public static void main(String[] args) {
+	    char chara = 'A';
+		for(int i= 1; i<= 5; i++) {
+			for(int j= 1; j<= i; j++) {
+			    
+				System.out.print(chara);
+				chara++;
+			}
+			System.out.println(" ");
+		}
+    }
+}
+*/
