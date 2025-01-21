37b57c563d8ecbdc1b7611f803490b715f34efe9
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-01-22 01:52:36 +0530

    Update Main.java

--- patterns/Main.java
+++ patterns/Main.java
@@ -146,3 +146,42 @@ class Main {
 }
 
 */
+
+
+/*
+
+Hollow Rectangle
+
+***** 
+*   * 
+*   * 
+***** 
+
+import java.util.*;
+
+class Main {
+    public static void HollowRectangle(int row , int col){
+        for(int i= 1; i<= row; i++){
+            for(int j= 1; j<= col; j++){
+                if(i==1 || i==row || j == 1 || j == col){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        HollowRectangle(row, col);
+        
+        
+    }
+}
+
+
+*/
