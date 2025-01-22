f967babcfa0859bcd18461e52d8d614c9b4dbd84
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-01-23 02:38:12 +0530

    Update Main.java

--- Collections/Main.java
+++ Collections/Main.java
@@ -1,1 +1,14 @@
 
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);//0
+        list.add(3);//1
+        list.add(80);//2
+        System.out.println(list.get(2));//80
+        System.out.println(list.size());//3
+        
+    }
+}
