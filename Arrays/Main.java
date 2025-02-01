676829d1251906ca892107e991f65a3fb39ff0d5
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-02-01 17:28:54 +0530

    Update Main.java

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -38,6 +38,8 @@ class Main {
         System.out.println(nums);
     }
 
+
+
     public static void main(String[] args) {
         Vector<Integer> nums = new Vector<>(Arrays.asList(1, 2, 3, 4, 5, 6));
         rev(nums);
@@ -45,3 +47,60 @@ class Main {
 }
 
 */
+
+
+/*
+Question 1: Given an integer array nums, return true if any value appears at least twice in the 
+array, and return false if every element is distinct. 
+Example 1: 
+Input: 
+nums = [1, 2, 3, 1] 
+Output: true 
+Example 2: 
+Input: 
+nums = [1, 2, 3, 4] 
+Output: false 
+Example 3: 
+Input: 
+nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2] 
+Output: true 
+Constraints: 
+•  1 <= nums . lengtth <= 10^5 
+•  -10^9 <= nums [ i ] <= 10^9 
+
+class Main {
+    public boolean leastTwice(int nums[], int n){
+    
+        for(int i= 0; i< n; i++){
+            for(int j= i+1; j< n; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+            return false;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 4};
+        int n = nums.length;
+        Main m = new Main();
+        System.out.println(m.leastTwice(nums, n));
+    }
+}
+
+using hashSet...
+class Main {
+    public boolean leastTwice(int nums[], int n){
+        HashSet <Integer> set = new HashSet<>();
+        for(int i= 0; i< n; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+
+*/
+
