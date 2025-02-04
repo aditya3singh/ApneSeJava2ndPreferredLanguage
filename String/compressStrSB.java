25c63d76ece097e97e15f180c6110bf064f7ba0e
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-02-05 01:19:02 +0530

    Create compressStrSB.java

---
+++ String/compressStrSB.java
@@ -0,0 +1,24 @@
+public class compressStrSB {
+    public static String compress(String str){
+        StringBuilder sb = new StringBuilder("");
+        for(int i= 0; i< str.length(); i++){
+            Integer cnt = 1;
+            while(i< str.length() - 1 && str.charAt(i) == str.charAt(i + 1)){
+                cnt++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(cnt > 1){
+                sb.append(cnt.toString());
+            }
+        }
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) {
+        String str = "aaabbcccdd";
+        System.out.println(compress(str));
+        
+        
+    }
+}
