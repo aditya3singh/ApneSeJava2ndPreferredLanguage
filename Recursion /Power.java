7e779d363dd61be35ab3c74527d1845d4f057530
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-02-12 17:28:42 +0530

    Update Power.java

--- Recursion /Power.java
+++ Recursion /Power.java
@@ -12,3 +12,25 @@ public class Power {
         System.out.println(power(x, n));
     }
 }
+
+
+// "static void main" must be defined in a public class.
+public class Main {
+    
+    public static int Optimizedpower(int x, int n){
+        if(n == 0) return 1;
+        int callThis = Optimizedpower(x, n/2);
+        int halfPower = callThis * callThis;
+        
+        if(n % 2 != 0){
+            halfPower = x *  halfPower;
+        }
+        
+        return  halfPower;
+    }
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+        System.out.println(Optimizedpower(x, n));
+    }
+}
