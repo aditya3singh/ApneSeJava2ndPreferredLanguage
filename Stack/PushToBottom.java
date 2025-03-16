6b72449580f00364e8aa07baa0bc390330291870
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-03-16 13:48:53 +0530

    Create PushToBottom.java

---
+++ Stack/PushToBottom.java
@@ -0,0 +1,30 @@
+// "static void main" must be defined in a public class.
+import java.util.*;
+
+public class PushToBottom {
+    
+    public static void pushAtbottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        
+        int top = s.pop();
+        pushAtbottom(s, data);
+        s.push(top);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        
+        pushAtbottom(s, 4);
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+}
