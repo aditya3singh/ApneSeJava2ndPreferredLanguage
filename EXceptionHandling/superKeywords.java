1aa7b7e20ad090cea1b30c0b460aac8b1ca27ca9
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-03-26 07:34:25 +0530

    Create superKeywords.java

---
+++ EXceptionHandling/superKeywords.java
@@ -0,0 +1,31 @@
+//to invoke the parents class variable 
+/*
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Animal{
+    String color = "white";
+}
+
+class Dog extends Animal{
+    String color = "black";
+    void print(){
+        System.out.println(color);//prints the color of dog 
+        System.out.println(super.color);//prints the color of the Animals
+    }
+}
+
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Dog d = new Dog();
+        d.print();
+        
+    }
+}
+*/
+
+//
